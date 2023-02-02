58c676a48b9d8b0bd285c508cd739700347cde06
nimoc <nimocc@126.com>
2023-02-02 18:41:13 +0800

    Redirect

--- context.go
+++ context.go
@@ -10,22 +10,24 @@ import (
 	"net/http"
 	"reflect"
 )
+
 // 包含 *http.Request http.ResponseWriter 并封装一些便捷方法
 type Context struct {
-	Writer http.ResponseWriter
-	Request *http.Request
-	router *Router
+	Writer        http.ResponseWriter
+	Request       *http.Request
+	router        *Router
 	resolvedParam bool
-	param map[string]string
+	param         map[string]string
 }
-func NewContext (w http.ResponseWriter, r *http.Request, router *Router) *Context {
+
+func NewContext(w http.ResponseWriter, r *http.Request, router *Router) *Context {
 	return &Context{
-		Writer: w,
+		Writer:  w,
 		Request: r,
-		router: router,
+		router:  router,
 	}
 }
-func (c *Context) RequestRealIP()  (ip string) {
+func (c *Context) RequestRealIP() (ip string) {
 	return realip.FromRequest(c.Request)
 }
 
@@ -33,6 +35,7 @@ func (c *Context) RequestRealIP()  (ip string) {
 func (c *Context) RequestContext() context.Context {
 	return c.Request.Context()
 }
+
 // 获取格式为: /user/{userID} URL的参数
 // 列如 /user/11 通过 c.Param("userID") 获取
 func (c *Context) Param(name string) (param string, err error) {
@@ -52,6 +55,7 @@ func (c *Context) Param(name string) (param string, err error) {
 func (c *Context) WriteStatusCode(statusCode int) {
 	c.Writer.WriteHeader(statusCode)
 }
+
 // 等同于 writer.Write(data) ,但函数签名返回 error 不返回 int
 func (c *Context) WriteBytes(b []byte) error {
 	_, err := c.Writer.Write(b)
@@ -60,13 +64,18 @@ func (c *Context) WriteBytes(b []byte) error {
 	}
 	return nil
 }
+
 // 设置 header Content-Type text/html; charset=UTF-8 并输出 buffer
 func (c *Context) Render(render func(buffer *bytes.Buffer) error) error {
 	buffer := bytes.NewBuffer(nil)
-	err := render(buffer) ; if err != nil {return err}
+	err := render(buffer)
+	if err != nil {
+		return err
+	}
 	c.Writer.Header().Set("Content-Type", "text/html; charset=UTF-8")
 	return c.WriteBytes(buffer.Bytes())
 }
+
 // 响应 json
 func (c *Context) WriteJSON(v interface{}) error {
 	data, err := xjson.Marshal(v)
@@ -76,6 +85,7 @@ func (c *Context) WriteJSON(v interface{}) error {
 	c.Writer.Header().Set("Content-Type", "application/json")
 	return c.WriteBytes(data)
 }
+
 // 绑定请求，支持自定义结构体表 `query` `form` `param`
 func (c *Context) BindRequest(ptr interface{}) error {
 	return BindRequest(ptr, c.Request)
@@ -87,12 +97,14 @@ func (c *Context) UnmarshalJSONFromQuery(queryKey string, ptr interface{}) error
 	}
 	value := c.Request.URL.Query().Get(queryKey)
 	if len(value) != 0 {
-		err := xjson.Unmarshal([]byte(value), ptr) ; if err != nil {
+		err := xjson.Unmarshal([]byte(value), ptr)
+		if err != nil {
 			return err
 		}
 	}
 	return nil
 }
+
 // 让 Router{}.OnCatchError 处理传入的错误
 func (c *Context) CheckPanic(r interface{}) {
 	err := c.router.OnCatchPanic(c, r)
@@ -100,6 +112,7 @@ func (c *Context) CheckPanic(r interface{}) {
 		panic(err)
 	}
 }
+
 // 让 Router{}.OnCatchError 处理传入的错误
 func (c *Context) CheckError(err error) {
 	err = c.router.OnCatchError(c, err)
@@ -128,4 +141,8 @@ func (c *Context) ClearCookie(cookie *http.Cookie) {
 	// -1 标识清除cookie
 	cookie.MaxAge = -1
 	http.SetCookie(c.Writer, cookie)
+}
+func (c *Context) Redirect(url string, code int) (err error) {
+	http.Redirect(c.Writer, c.Request, url, code)
+	return
 }
